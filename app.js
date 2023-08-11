a49cfa5091d760de36d0e21d53c2eab8eeb936f4
Francisca Guerra <franciscaguerra@Macbook-Fran-Guerra.local>
2023-08-11 11:50:54 -0400

    push last version

--- app.js
+++ app.js
@@ -8,29 +8,26 @@ const port = 4000
 app.use(express.json(), cors())
 
 const conn = createPool({
-  host: 'beyxl2fdnpiory1jhqi7-mysql.services.clever-cloud.com',
-  database: 'beyxl2fdnpiory1jhqi7',
-  user: 'u78sao3di83yranz',
-  password: 'u78sao3di83yranz',
-  port: '3306'
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORT
 })
 
 app.post('/signup', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+  console.log(username, password)
   conn.query(
-    `INSERT INTO users_db (username, password) VALUES (?, ?)`,
+    "INSERT INTO `users_db` (`username`, `password`) VALUES (?, ?)",
     [username, password],
-    (err, rows) => {
-      console.log("ROWS", rows)
+    (err, result) => {
       if (err) {
-        console.log("Ingreso a error")
-        res.send(err)       
-      } else {
-        console.log("No ingreso a error")
-        console.log(rows)
-        res.sendStatus(200);
+        console.log(err)
+        return res.json({Error: "Inserting data Error in server"})
       }
+      return res.json("Success") 
     })
 })
 
@@ -39,16 +36,14 @@ app.get('/login', (req, res) => {
   const password = req.query.password;
   console.log(username, password)
   conn.query(
-    'SELECT * FROM users_db WHERE username = ? and password = ?',
+    "SELECT * FROM `users_db` WHERE username = ? and password = ?",
     [username, password],
-    (err, rows) => {
-      console.log("ROWS", rows)
-      if (err) {
-        res.send(err)       
-      } else {
-        console.log(rows)
-        rows.length > 0 ? res.sendStatus(200) : res.status(401).send("Usuario no encontrado.");
-      }
+    (err, data) => {
+      if (err){
+        console.log(err)
+        return res.json({Error: "User not found"})}
+      console.log(data)
+      return res.json(data) 
     })
 })
 
